86f163c6d352a7eb8a047b0e758b530a7e634ea8
xfl <xfl@ncmem.com>
2017-06-26 10:13:59 +0800

    优化初始化数据库逻辑

--- demoSql2005/db/sql.aspx
+++ demoSql2005/db/sql.aspx
@@ -1,1 +1,17 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="sql.aspx.cs" Inherits="up6.demoSql2005.db.sql" %>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form action="sql.aspx" method="post">
+        数据库连接信息：<br/>
+        User Id:        <input type="text" name="uid" value="<%=this.m_dbUser %>" /><br/>
+        Password:       <input type="text" name="pwd" value="<%=this.m_dbPass %>" /><br/>
+        Initial Catalog:<input type="text" name="dbname" value="<%=this.m_dbName %>" /><br/>
+        <input type="submit" value="提交" />
+    </form>
+</body>
+</html>
--- demoSql2005/db/sql.aspx.cs
+++ demoSql2005/db/sql.aspx.cs
@@ -7,14 +7,17 @@ using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml;
 
 namespace up6.demoSql2005.db
 {
     public partial class sql : System.Web.UI.Page
     {
-        private string m_dbName;	//数据库名称
+        public string m_dbName;	//数据库名称
+        public string m_dbUser;
+        public string m_dbPass;
 
-        public string GetConStr()
+        /*public string GetConStr()
         {
             var str = System.Configuration.ConfigurationManager.ConnectionStrings["sql2005"];
             string cs = str.ConnectionString;
@@ -35,7 +38,7 @@ namespace up6.demoSql2005.db
             return rs;
         }
 
-        public void createDataBase() 
+        public string createDataBase() 
         {
             SqlConnection con = new SqlConnection(this.GetConStr());
             SqlCommand cmd = con.CreateCommand();
@@ -48,7 +51,8 @@ namespace up6.demoSql2005.db
             cmd.Connection.Close();
 
             createTable();
-        }
+            return "";
+        }*/
 
         public void createTable() 
         {
@@ -132,10 +136,142 @@ namespace up6.demoSql2005.db
             }
         }
 
+        public void showConfig()
+        {
+            var str = System.Configuration.ConfigurationManager.ConnectionStrings["sql2005"];
+            string cs = str.ConnectionString;
+            readConfig(cs);
+        }
+
+        public void readConfig(string str) 
+        {
+            string ic = "Initial Catalog";
+            string uid = "User Id";
+            string password = "Password";
+            List<string> lst = new List<string>();
+            lst.Add(ic);
+            lst.Add(uid);
+            lst.Add(password);
+
+            foreach (string s in lst)
+            {
+                int index = str.IndexOf(s);
+                int start = index + s.Length + 1;
+
+                int end = str.IndexOf(";", start);
+                string name = str.Substring(start, end - start);
+
+                if (string.Equals(s, ic))
+                {
+                    this.m_dbName = name;
+                }
+                else if (string.Equals(s, uid))
+                {
+                    this.m_dbUser = name;
+                }
+                else
+                {
+                    this.m_dbPass = name;
+                }
+            }
+        }
+        
+        public void updateConfig(string id,string pwd,string dbname) 
+        {
+            XmlDocument doc = new XmlDocument();
+            string strFileName = AppDomain.CurrentDomain.BaseDirectory.ToString() + "Web.config";
+
+            doc.Load(strFileName);
+
+            bool result = false;
+            XmlNodeList nodes = doc.GetElementsByTagName("add");
+            for (int i = 0; i < nodes.Count; i++)
+            {   
+                XmlAttribute name = nodes[i].Attributes["name"];
+                if (name != null)
+                {
+                    if (name.Value == "sql2005")
+                    {
+                        name = nodes[i].Attributes["connectionString"];
+                        string str = name.Value;
+                        name.Value = replaceAll(str,id,pwd,dbname);
+                        
+                        if(!string.Equals(str,name.Value))
+                        {
+                            result = true;
+                        }
+                        break;
+                    }
+                }
+            }
+            if (result)
+            {
+                doc.Save(strFileName);
+            }
+        }
+
+        public string replaceAll(string str,string id,string pwd,string dbname)
+        {
+            string ic = "Initial Catalog";
+            string uid = "User Id";
+            string password = "Password";
+            List<string> lst = new List<string>();
+            lst.Add(ic);
+            lst.Add(uid);
+            lst.Add(password);
+
+            foreach(string s in lst)
+            {
+                int index = str.IndexOf(s);
+                int start = index + s.Length + 1;
+
+                int end = str.IndexOf(";", start);
+                string name = str.Substring(start, end - start);
+
+                if(string.Equals(s,ic))
+                {
+                    str = str.Replace(name, dbname);
+                }
+                else if (string.Equals(s, uid))
+                {
+                    str = str.Replace(name, id);
+                }
+                else 
+                {
+                    str = str.Replace(name, pwd);
+                }
+            }
+            return str;
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            createDataBase();
-            Response.Write("创建成功");
+            showConfig();
+
+            string id = Request.Form["uid"];
+            string pwd = Request.Form["pwd"];
+            string dbname = Request.Form["dbname"];
+
+            if (id != null)
+            {
+                this.m_dbUser = id;
+            }
+            if (pwd != null)
+            {
+                this.m_dbPass = pwd;
+            }
+            if (dbname != null)
+            {
+                this.m_dbName = dbname;
+            }
+            updateConfig(this.m_dbUser,this.m_dbPass,this.m_dbName);
+
+            if(id!= null || pwd!= null || dbname!= null)
+            {
+                createTable();
+            }
+            //createDataBase();
+            //Response.Write("创建成功");
         }
     }
 }
