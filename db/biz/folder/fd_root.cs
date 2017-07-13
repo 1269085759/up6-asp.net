1bb1aa4a45951a49b2f3799d2a023e2b85ac3380
qwl <qwl@ncmem.com>
2017-07-13 16:50:51 +0800

    更新根目录结构

--- db/biz/folder/fd_root.cs
+++ db/biz/folder/fd_root.cs
@@ -6,9 +6,14 @@ namespace up6.db.biz.folder
     /// <summary>
     /// 文件夹
     /// </summary>
-    public class fd_root : fd_child
+    public class fd_root : FileInf
     {
-        public List<fd_child> folders;
+        public List<FileInf> folders;
         public List<FileInf> files;
+
+        public fd_root()
+        {
+            this.fdTask = true;
+        }
     }
 }
