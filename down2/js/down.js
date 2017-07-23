0db55a17a00229e32fe3bea421c64eb127d8a32e
qwl <qwl@ncmem.com>
2017-07-23 13:56:33 +0800

    下载控件，版本号更新

--- down2/js/down.js
+++ down2/js/down.js
@@ -44,7 +44,7 @@ function DownloaderMgr()
 		, "Debug"		: false//调试模式
 		, "LogFile"		: "f:\\log.txt"//日志文件路径。
 		, "Company"		: "荆门泽优软件有限公司"
-		, "Version"		: "1,2,56,31650"
+		, "Version"		: "1,2,58,51170"
 		, "License"		: ""//
 		, "Cookie"		: ""//
 		, "ThreadCount"	: 1//并发数
