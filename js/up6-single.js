c556d872441ebd9ac1ea186ca6f7c4d70b869655
qwl <qwl@ncmem.com>
2019-12-24 16:46:02 +0800

    单文件示例更新

--- js/up6-single.js
+++ js/up6-single.js
@@ -255,28 +255,17 @@ function HttpUploaderMgr()
 	    }
 	    if (this.firefox)
 	    {
-	        //if (!this.app.checkFF() || parseInt(this.ffVer[1]) >= 50)//仍然支持npapi
-            {
-                this.edge = true;
-                this.app.postMessage = this.app.postMessageEdge;
-                this.edgeApp.run = this.edgeApp.runChr;
-            }
+            this.edge = true;
+            this.app.postMessage = this.app.postMessageEdge;
+            this.edgeApp.run = this.edgeApp.runChr;
         }
         else if (this.chrome)
         {
             this.app.check = this.app.checkFF;
             jQuery.extend(this.Config.firefox, this.Config.chrome);
-            //_this.Config["XpiPath"] = _this.Config["CrxPath"];
-            //_this.Config["XpiType"] = _this.Config["CrxType"];
-            //44+版本使用Native Message
-            //if (parseInt(this.chrVer[1]) >= 44) {
-                //_this.firefox = true;
-                //if (!this.app.checkFF())//仍然支持npapi
-                {
-                    this.edge = true;
-                    this.app.postMessage = this.app.postMessageEdge;
-                    this.edgeApp.run = this.edgeApp.runChr;
-                }
+            this.edge = true;
+            this.app.postMessage = this.app.postMessageEdge;
+            this.edgeApp.run = this.edgeApp.runChr;
         }
         else if (this.edge) {
             this.app.postMessage = this.app.postMessageEdge;
