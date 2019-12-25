0f22b42c1947baa232bbf4a95a011bc46bbd6e74
qwl <qwl@ncmem.com>
2019-12-25 16:45:46 +0800

    增加mac,linux支持

--- js/up6-single.js
+++ js/up6-single.js
@@ -260,31 +260,32 @@ function HttpUploaderMgr()
 	    if (window.navigator.platform == "Win64")
 	    {
 	        jQuery.extend(this.Config.ie, this.Config.ie64);
-	    }
-	    if (this.firefox)
+	    }//macOS
+        else if (window.navigator.platform == "MacIntel") {
+            this.edge = true;
+            this.app.postMessage = this.app.postMessageEdge;
+            this.edgeApp.run = this.edgeApp.runChr;
+            this.Config.exe.path = this.Config.mac.path;
+        }
+        else if (window.navigator.platform == "Linux x86_64") {
+            this.edge = true;
+            this.app.postMessage = this.app.postMessageEdge;
+            this.edgeApp.run = this.edgeApp.runChr;
+            this.Config.exe.path = this.Config.linux.path;
+        }
+	    else if (this.firefox)
 	    {
-	        //if (!this.app.checkFF() || parseInt(this.ffVer[1]) >= 50)//仍然支持npapi
-            {
-                this.edge = true;
-                this.app.postMessage = this.app.postMessageEdge;
-                this.edgeApp.run = this.edgeApp.runChr;
-            }
+            this.edge = true;
+            this.app.postMessage = this.app.postMessageEdge;
+            this.edgeApp.run = this.edgeApp.runChr;
         }
         else if (this.chrome)
         {
             this.app.check = this.app.checkFF;
             jQuery.extend(this.Config.firefox, this.Config.chrome);
-            //_this.Config["XpiPath"] = _this.Config["CrxPath"];
-            //_this.Config["XpiType"] = _this.Config["CrxType"];
-            //44+版本使用Native Message
-            //if (parseInt(this.chrVer[1]) >= 44) {
-                //_this.firefox = true;
-                //if (!this.app.checkFF())//仍然支持npapi
-                {
-                    this.edge = true;
-                    this.app.postMessage = this.app.postMessageEdge;
-                    this.edgeApp.run = this.edgeApp.runChr;
-                }
+            this.edge = true;
+            this.app.postMessage = this.app.postMessageEdge;
+            this.edgeApp.run = this.edgeApp.runChr;
         }
         else if (this.edge) {
             this.app.postMessage = this.app.postMessageEdge;
