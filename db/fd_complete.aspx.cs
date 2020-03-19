6f027fe2473476dbf12058ae6cce40ebce7d30d5
qwl <qwl@ncmem.com>
2020-03-19 15:54:16 +0800

    文件夹上传完毕逻辑增加覆盖同名目录功能

--- db/fd_complete.aspx.cs
+++ db/fd_complete.aspx.cs
@@ -14,6 +14,7 @@ namespace up6.db
             string id = this.reqString("id");
             string uid = this.reqString("uid");
             string cak = this.reqString("callback");
+            int cover = this.reqToInt("cover");
             int ret = 0;
 
             if ( string.IsNullOrEmpty(id) || 
@@ -24,6 +25,14 @@ namespace up6.db
             {
                 DbFolder db = new DbFolder();
                 FileInf folder = db.read(id);
+                FileInf fdExist = db.read(folder.pathRel, folder.pid,id);                
+                if(1==cover && fdExist !=null)
+                {
+                    folder.id = fdExist.id;
+                    folder.pid = fdExist.pid;
+                    folder.pidRoot = fdExist.pidRoot;
+                }
+
                 //根节点
                 FileInf root = new FileInf();
                 root.id = folder.pidRoot;
@@ -36,12 +45,27 @@ namespace up6.db
                 //扫描文件夹结构，
                 fd_scan sa = new fd_scan();
                 sa.root = root;//
+
+                //清理同名子文件
+                if (1 == cover && fdExist!=null)
+                {
+                    //覆盖同名子文件
+                    sa.cover(folder, folder.pathSvr);
+                }
+
+                //添加文件记录
                 sa.scan(folder,folder.pathSvr);
 
                 //更新扫描状态
                 DBFile.fd_scan(id, uid);
 
                 up6_biz_event.folder_post_complete(id);
+
+                //删除当前目录
+                if (1 == cover && fdExist != null)
+                {
+                    DBFolder.del(id, int.Parse(uid));
+                }
 
                 ret = 1;
             }
