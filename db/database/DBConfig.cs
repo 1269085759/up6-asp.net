73c2b618db6ab81029c2f99b7e02cc8175372bb4
xfl <xfl@ncmem.com>
2020-04-17 17:34:13 +0800

    完善配置使用oracle逻辑

--- db/database/DBConfig.cs
+++ db/database/DBConfig.cs
@@ -13,45 +13,50 @@ namespace up6.db.database
     {
         public string m_db = "sql";
 
+        public bool isOracle()
+        {
+            return string.Compare(this.m_db, "oracle") == 0;
+        }
+
         public un_builder ub()
         {
-            if (string.Compare(this.m_db,"oracle") == 0) return new un_builder_oracle();
+            if (isOracle()) return new un_builder_oracle();
             else return new un_builder();
         }
 
         public DBFile db()
         {
-            if (string.Compare(this.m_db, "oracle") == 0) return new DBFileOracle();
+            if (isOracle()) return new DBFileOracle();
             else return new DBFile();
         }
 
         public DnFile dn()
         {
-            if (string.Compare(this.m_db, "oracle") == 0) return new DnFileOracle();
+            if (isOracle()) return new DnFileOracle();
             else return new DnFile();
         }
 
         public DnFolder dnf()
         {
-            if (string.Compare(this.m_db, "oracle") == 0) return new DnFolderOracle();
+            if (isOracle()) return new DnFolderOracle();
             else return new DnFolder();
         }
 
         public fd_scan sa()
         {
-            if (string.Compare(this.m_db, "oracle") == 0) return new fd_scan_oracle();
+            if (isOracle()) return new fd_scan_oracle();
             else return new fd_scan();
         }
 
         public SqlExec ec()
         {
-            if (string.Compare(this.m_db, "oracle") == 0) return new OracleExec();
+            if (isOracle()) return new OracleExec();
             else return new SqlExec();
         }
 
         public DbBase bs()
         {
-            if (string.Compare(this.m_db, "oracle") == 0) return new OracleDbBase();
+            if (isOracle()) return new OracleDbBase();
             else return new DbBase();
         }
 
